835eb4baf15e9b3c4301fb8a60c3ba22816d969f
Iskander_Ismagilov <isma.translator@gmail.com>
2018-02-04 19:57:44 +0500

    Avereger is ready to use!

--- Project/Avereger/Program.cs
+++ Project/Avereger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Avereger
 {
@@ -6,7 +8,18 @@ namespace Avereger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isNum=true;
+            List<int> nums = new List<int>();
+            while (isNum)
+            {
+                Console.Write("Enter a number or any key to calculate an averege number: ");
+                string input = Console.ReadLine();
+                isNum = int.TryParse(input, out int num);
+                if (isNum) nums.Add(num);
+                else if (nums.Count > 0) Console.WriteLine($"The averege number is: {nums.Average()}");                
+            }
+            Console.WriteLine("Bye!");
+            Console.ReadLine();
         }
     }
 }
