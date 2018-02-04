38562573eea5e1cd69d738f4839560043e1101eb
Iskander_Ismagilov <isma.translator@gmail.com>
2018-02-04 19:44:28 +0500

    First commit

---
+++ Project/Avereger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Avereger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
